8cd7a75f6ca2eb1789aced59d7d1a24da28c447c
MaryamCodeHub <maryamnaseem833@gmail.com>
2024-09-08 05:17:07 -0700

    Update Internship_project_1.py

--- Internship_project_1.py
+++ Internship_project_1.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 class RbGame:
     def __init__(self, num_red, num_blue, version, first_player, depth):
         self.num_red = num_red
@@ -88,7 +89,7 @@ class RbGame:
                         best_move = (color, num_marbles)
         self.remove_marbles(best_move[0], best_move[1])
         print("Computer removes {} {} marbles.".format(best_move[1], best_move[0]))
-        
+
     def play_game(self):
         current_player = self.first_player
         while not self.is_game_over():
@@ -97,30 +98,53 @@ class RbGame:
                 color = input("Enter 'red' or 'blue' to remove marbles: ")
                 num_marbles = self.get_valid_input("Enter 1 or 2 to remove marbles: ")
                 if num_marbles > self.board[color]:
-                    print("You can't remove that many marbles. There are only {} {} marbles left.".format(self.board[color], color))
+                    print("You can't remove that many marbles. There are only {} {} marbles left.".format(
+                        self.board[color], color))
                     continue
                 self.remove_marbles(color, num_marbles)
             else:
                 self.computer_move()
-            if self.board["red"] == 0:
-                print("Game over! You removed all red marbles.")
-                break
             current_player = "computer" if current_player == "human" else "human"
+
+        self.print_board()
         self.calculate_score()
         print("Your final score is:", self.score)
+
+        # Announce the winner based on the game version
+        if self.version == "standard":
+            if self.board["red"] == 0 or self.board["blue"] == 0:
+                winner = "human" if current_player == "computer" else "computer"
+                print(f"Game over! The winner is {winner}.")
+        else:  # Misere version
+            if self.board["red"] == 0 or self.board["blue"] == 0:
+                winner = "computer" if current_player == "computer" else "human"
+                print(f"Game over! The winner is {winner}.")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Red-Blue Nim Game")
     parser.add_argument("--num-red", type=int, default=10, help="Number of red marbles")
     parser.add_argument("--num-blue", type=int, default=10, help="Number of blue marbles")
-    parser.add_argument("--version", choices=["standard", "misere"], default="standard", help="Game version")
     parser.add_argument("--first-player", choices=["human", "computer"], default="human", help="First player")
     parser.add_argument("--depth", type=int, default=5, help="Search depth for AI")
 
     args = parser.parse_args()
 
-    game = RbGame(args.num_red, args.num_blue, args.version, args.first_player, args.depth)
+    # Prompt user to choose the game version
+    while True:
+        version_choice = input("Choose game version: Press 1 for Standard, 2 for Misere: ")
+        if version_choice == '1':
+            version = "standard"
+            break
+        elif version_choice == '2':
+            version = "misere"
+            break
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+    game = RbGame(args.num_red, args.num_blue, version, args.first_player, args.depth)
     game.play_game()
+
 
 if __name__ == "__main__":
     main()
